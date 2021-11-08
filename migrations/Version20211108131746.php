3b8ccbf6c779536756ff1c38dabbc7b13c58cbe1
debe0025 <deb.jules@outlook.fr>
2021-11-08 14:24:49 +0100

    fix migration error due to php 8 with new method

--- migrations/Version20211108131746.php
+++ migrations/Version20211108131746.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211104153646 extends AbstractMigration
+final class Version20211108131746 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20211104153646 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE activity (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, name VARCHAR(255) NOT NULL, duration INT NOT NULL, INDEX IDX_AC74095A7E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, name VARCHAR(255) NOT NULL, is_activated TINYINT(1) NOT NULL, `repeat` INT NOT NULL, start DATE NOT NULL, message LONGTEXT DEFAULT NULL, INDEX IDX_527EDB257E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, name VARCHAR(255) NOT NULL, is_activated TINYINT(1) NOT NULL, repeat_interval INT NOT NULL, start DATETIME NOT NULL, message LONGTEXT DEFAULT NULL, INDEX IDX_527EDB257E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095A7E3C61F9 FOREIGN KEY (owner_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB257E3C61F9 FOREIGN KEY (owner_id) REFERENCES `user` (id)');
@@ -35,5 +35,10 @@ final class Version20211104153646 extends AbstractMigration
         $this->addSql('DROP TABLE activity');
         $this->addSql('DROP TABLE task');
         $this->addSql('DROP TABLE `user`');
+    }
+
+    public function isTransactional(): bool
+    {
+        return false;
     }
 }
