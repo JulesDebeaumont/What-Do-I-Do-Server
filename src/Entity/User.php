fbb344287032e6c6b8297e42dbf5d6f64c852d8c
debe0025 <deb.jules@outlook.fr>
2021-11-08 15:40:19 +0100

    update swagger with jwt decorator

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,8 +8,6 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use ApiPlatform\Core\Annotation\ApiResource;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
