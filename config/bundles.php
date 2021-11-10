68896f26479cec7d0b6f0031a7cd887240a8206f
debe0025 <deb.jules@outlook.fr>
2021-11-10 03:27:27 +0100

    install refresh token + new table for token

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
